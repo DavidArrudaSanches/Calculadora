c8685d4f32afaa6fd8b16f59510257608d4cdbbf
David Sanches <david.sanches2750@gmail.com>
2024-09-17 11:29:29 -0300

    Add:Barra de carregamento Add:Forms Calculadora

--- Form1.cs
+++ Form1.cs
@@ -9,8 +9,22 @@ namespace Calculadora
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            
-         
+            progressBar1.Value += 1;
+            if (progressBar1.Value == 10)
+            {
+                this.Hide();
+                Calculadora calculadora = new Calculadora();
+                calculadora.Show();
+                timer1.Enabled = false;
+            }
+            if (progressBar1.Value == 9)
+            {
+                pictureBox2.Visible = true;
+                
+                pictureBox1 .Visible = false;
+
+            }
+
         }
 
         private void progressBar1_Click(object sender, EventArgs e)
@@ -20,7 +34,7 @@ namespace Calculadora
 
         private void pictureBox2_Click(object sender, EventArgs e)
         {
-        
+
             }
     }
 }
